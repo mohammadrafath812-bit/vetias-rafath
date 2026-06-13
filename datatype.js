6655abe64cc6a2cb426226effb0c096be2dd94b1
kanagarajpalani150-afk <kanagarajpalani@gmail.com>
2026-06-13 13:57:03 +0530

    added looping.js

---
+++ datatype.js
@@ -0,0 +1,52 @@
+console.log("welcome to primitive data types");
+
+//number 
+console.log("DT number");
+var age = 30;
+console.log(age);
+
+//spring
+console.log("DT string");
+var name ="john";
+console.log(name);
+
+//boolean
+console.log("DT null");
+var is student ="true"
+console.log(is student);
+
+//null
+console.log("DT null");
+var empty value =null ;
+console.log(empty value);
+
+//undefined
+console.log("DT undefined");
+var undefined value;
+console.log(undefined value);
+
+//symbol
+console.log("DT symbols");
+var symbol value=symbol("symbol");
+console.log(symbol value);
+
+//big int
+console.log("DT big int");
+var big int value =1234..n
+console.log(big int);
+console.log("wol to non PD");
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+
+
+
+
---
+++ looping.js
@@ -0,0 +1,14 @@
+// for loop
+for (let i=0;i < 10;i++) {
+    console.log(i + "x 2= " + i * 2);
+}
+    
+
+
+
+
+
+
+
+
+
